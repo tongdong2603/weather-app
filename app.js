9ebfbd30d610c15a8761d4b1865b55c0aab15c7f
Tong Dong <tongdong2603@gmail.com>
2019-11-06 14:57:41 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const request = require("request");
+
+const url =
+  "https://api.darksky.net/forecast/479db41ca2fea983dfa294c5ee33bb3c/37.8267,-122.4233";
+request({ url: url, json: true }, (err, res) => {
+  console.log("test: ", res.body.daily.data[0]);
+});
+const geocurl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidG9uZ2RvbmciLCJhIjoiY2sybXdzOHgyMG1ubDNobHA4ZmtoODB3NSJ9.UysRzfWfLX0ldAeSZWVg0A&limit=1";
+request({ url: geocurl, json: true }, (err, res) => {
+  console.log("test đâs: ", res.body.features[0].center);
+  const  latitude = res.body.features[0].center[1]
+  const  longitude = res.body.features[0].center[0]
+});
